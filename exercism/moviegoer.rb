ee01fe10475bb1fae79ff942af33b2e3a6900501
Wendy Wu <wendy.wu@abagile.com>
2023-05-08 10:24:11 +0800

    Exercism Moviegoer
    ref: https://exercism.org/tracks/ruby/exercises/moviegoer

---
+++ exercism/moviegoer.rb
@@ -0,0 +1,23 @@
+# https://exercism.org/tracks/ruby/exercises/moviegoer
+class NotMovieClubMemberError < RuntimeError
+end
+
+class Moviegoer
+  def initialize(age, member: false)
+    @age = age
+    @member = member
+  end
+
+  def ticket_price
+    @age >= 60 ? 10 : 15
+  end
+
+  def watch_scary_movie?
+    @age >= 18
+  end
+
+  # Popcorn is 🍿
+  def claim_free_popcorn!
+    @member ? "🍿" : (raise NotMovieClubMemberError)
+  end
+end
